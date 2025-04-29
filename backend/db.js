b7d3ad3a5c344f070e4d50e270b7915525c7ad45
demah09 <deemah0909@gmail.com>
2025-04-29 19:34:20 +0300

    Removed hardcoded password, added .env support

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,18 @@
-// Database connection
+// Load environment variables from .env file for the DB pass
+require('dotenv').config();
 
+const fs = require("fs"); //for reading the SSL certificate
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "checkmate-spdb.l.aivencloud.com",  user: "avnadmin",
-  password: "AVNS_o1GhUbRe6GHgkyTGG9O",
+  host: "checkmate-spdb.l.aivencloud.com",
+  user: "avnadmin",
+  password: process.env.DB_PASSWORD, 
   database: "defaultdb",
-  port: 25060, // Default Aiven MySQL port
+  port: 25060,
   ssl: {
-    ca: fs.readFileSync("./certs/ca.pem")  }
+    ca: fs.readFileSync("./certs/ca.pem") 
+  }
 });
 
 module.exports = db;
